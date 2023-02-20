21dabd8a6c77a954e7c508f8d1fe7c08e4829925
Alejandro Sanabria <alesanabriav@gmail.com>
2023-02-20 11:04:45 -0500

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Alejandro Sanabria on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
